9148a397352c72ee75e4d67292a90005743d9ce5
xlgp <zhang15733@sina.cn>
2019-11-13 21:50:29 +0800

    小改动

--- xlgp/r.js
+++ xlgp/r.js
@@ -1,12 +1,10 @@
 requirejs.config({
-    paths:{
         paths: {
             util: 'https://gitee.com/xlgp/static/raw/master/xlgp/xlgp.util',
             clipboard: 'https://cdn.jsdelivr.net/npm/clipboard@2/dist/clipboard.min',
             Vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue',
             XCVue: 'https://gitee.com/xlgp/static/raw/master/xlgp/xc-search-comp',
         }
-    }
 })
 
 require(['XCVue', 'util'], function(XCVue, util){
