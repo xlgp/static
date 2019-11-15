b29ed09320f96aa8d7845903cceb5930158a3719
xlgp <zhang15733@sina.cn>
2019-11-15 10:23:12 +0800

    新增数据格式化函数

--- xlgp/xc-search-comp.js
+++ xlgp/xc-search-comp.js
@@ -15,7 +15,7 @@ define([
     //创建search comp外层盒子
     let APP_BOX = 'xc-app-box';
     util.el(APP_BOX);
-    // util.loadAsync.css('css/xc-search-comp.css');
+    util.loadAsync.css('https://hzxcsy.oss-cn-hangzhou.aliyuncs.com/css/xc-search-comp.css');
     /**
      * 按钮组建,
      * @type {Object}
@@ -52,6 +52,11 @@ define([
             };
         },
         methods: {
+            /**
+             * 输入事件
+             * @param  {[type]} $event [description]
+             * @return {[type]}        [description]
+             */
             textHandInput: function ($event) {
                 let content = $event.target.innerText.trim();
                 this.isPlaceHolder = content === "" ? true : false;
@@ -77,13 +82,14 @@ define([
     let mixins = {
         data: function () {
             return {
-                resultList: [],
-                itemDict: {},
+                resultList: [], //table内数据
+                itemDict: {}, //表头字典
+                paramDict:[], //参数字典,即将arealist格式化成对象的key, 且key的顺序需与arealist字段一致
                 title: '标题',
-                areaList: [],
+                areaList: [], //输入框内容
                 style: {
                     bg: {
-                        'background-image': 'url(http://img.infinitynewtab.com/wallpaper/' + Math.floor((Math.random() * 3000) + 1) + '.jpg)',
+                        'background-image': 'url(//img.infinitynewtab.com/wallpaper/' + Math.floor((Math.random() * 3000) + 1) + '.jpg)',
                     }
                 },
                 btns: [],
@@ -115,8 +121,8 @@ define([
                 }
                 this.resultList.push(obj);
             },
-            xcInputHandler: function () {
-                console.log('xcInputHandler function in mixin');
+            xcBtnHandler: function () {
+                console.info('xcBtnHandler function in mixin');
             },
             getAreaText: function (areaList) {
                 this.areaList = areaList;
@@ -126,7 +132,33 @@ define([
             tabHeadList: function () { //表头
                 return Object.values(this.itemDict);
             },
-            itemList: function () {
+            parseList:function(){ //格式化 this.arealist
+                let len =  this.areaList.length;
+                let list = [];
+                for (var i = 0; i < len; i++) {
+                    list.push(this.areaList[i].trim().split('\t'));
+                }
+                return list;
+            },
+            paramList:function(){ //将arealist数据对象格式化
+                if (!this.paramDict || this.paramDict.length == 0) {
+                    console.error('paramDict不能为空');
+                    return;
+                }
+                let list = [];
+                if (this.paramDict.length != this.parseList[0].length) {
+                    console.info('数据与 paramDict key对应不一致');
+                }
+                for (var i = 0; i < this.parseList.length; i++) {
+                    let obj = {};
+                    for (var j = 0; j < this.paramDict.length; j++) {
+                       obj[this.paramDict[j]] = this.parseList[i][j];
+                    }
+                    list.push(obj);
+                }
+                return list;
+            },
+            itemList: function () { //表中项
                 return Object.keys(this.itemDict);
             },
             cpTbodyId: function () {
@@ -144,7 +176,7 @@ define([
         <div :id="id" class="xc-bg" :style="style.bg">
         <div class="xc-wapper">
             <div class="xc-title"><span>{{title}}</span></div>
-            <xc-button v-on:click="xcInputHandler" :btns="btns"></xc-button>
+            <xc-button v-on:click="xcBtnHandler" :btns="btns"></xc-button>
             <xc-textarea :placeholder="placeholder" @get-arealist="getAreaText"></xc-textarea>
             <div class="xc-table">
                 <table>
